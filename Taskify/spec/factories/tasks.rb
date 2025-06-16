322c4dc9abc7907ccb3373ffd171a71481de48dd
SantiagoChemello <chemello.santiago8@gmail.com>
2025-06-16 03:24:48 -0300

    repo config fix

--- Taskify/spec/factories/tasks.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :task do
-    title { Faker::Lorem.sentence(word_count: 3) }
-    description { Faker::Lorem.paragraph(sentence_count: 2) }
-    status { 'pending' }
-    priority { 'medium' }
-    association :user
-  end
-end 
